f963a5a8a6e09c545a5555a4c2899867aad99c3e
Nozdromu <yileiding1990@gmail.com>
2018-06-17 13:11:51 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Mylayout from './com/Mylayout/index.jsx';
+import 'antd/dist/antd.css';
+import 'ant-design-pro/dist/ant-design-pro.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <Mylayout />
+    );
+  }
+}
+
+export default App;
